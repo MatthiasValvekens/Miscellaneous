07795e1a0f480eaf6e7d6cc3f9de2499b5312cf5
Matthias Valvekens <matthias.valvekens@gmail.com>
2014-05-21 11:41:59 +0200

    Update knotcalc.py

--- knotcalc.py
+++ knotcalc.py
@@ -1,7 +1,11 @@
 from ast import literal_eval
-from knotdraw import save_link
 
+#Connection mappings
 inoutdic={0:3,3:0,1:2,2:1}
+f1conn={1:3,3:1,0:2,2:0}
+f2conn={0:1,1:0,3:2,2:3}
+loopdic={(0,2):(1,3),(1,3):(0,2),(0,1):(2,3),(2,3):(0,1)}
+
 seendic={}
 def joinstotuples(l):
 	return tuple((0 if i==0 else tuple(i)) for i in l)
@@ -10,12 +14,30 @@ def joinstolists(t):
 def lget(list,index,default=0):
 	if index<0 or index>=len(list): return default
 	else: return list[index]
+def connect(targlst,srclst,strand,strandb):
+	targlst[srclst[strand][0]]\
+	       [srclst[strand][1]]=srclst[strandb]
+def connect_all(targlst,srclst,mapping):
+	for a,b in mapping.items():
+		connect(targlst,srclst,a,b)
+def read_link(fname):
+	joins=[]
+	crossings=[]
+	with open(fname,'r') as infile:
+		for line in infile:
+			if ':' not in line or '[' not in line: continue
+			spl=line.split(':')
+			crossings.append(-1 if ('-' in spl[0]) else 1)
+			joins.append(literal_eval(spl[1].strip()))
+	return crossings,joins
+
 class LaurentPolynomial(object):
-	def __init__(self,coeff): #coeff centered around 0
+	def __init__(self,coeff):
 		self.coeff=coeff
 	def trim(self):
 		for i in range(len(self.coeff)//2):
-			if(self.coeff[i]!=0 or self.coeff[len(self.coeff)-1-i]!=0):
+			if(self.coeff[i]!=0 or \
+				self.coeff[len(self.coeff)-1-i]!=0):
 				break
 		return LaurentPolynomial(self.coeff[i:len(self.coeff)-i])
 	def __str__(self):
@@ -48,9 +70,10 @@ class LaurentPolynomial(object):
 		extrres=max(extr1,extr2)
 		res=LaurentPolynomial([0]*(2*extrres+1))
 		for i in range(-extrres,extrres+1):
-			res.coeff[i+extrres]=lget(self.coeff,i+extr1)+lget(other.coeff,i+extr2)
+			res.coeff[i+extrres]=\
+				lget(self.coeff,i+extr1)+lget(other.coeff,i+extr2)
 		return res.trim()
-	#multiply by a**p
+	#Multiply by a**p
 	def amul(self,p):
 		if p==0: return self
 		extr=len(self.coeff)//2
@@ -67,115 +90,61 @@ class LaurentPolynomial(object):
 
 	def __sub__(self,other):
 		return self.__add__(other.__neg__())
+
 class Link(object):
-	#0  1
-	# 
-	#2  3
-	#len(joins)==len(crossings)
-	#array of four-tuples that records where strands are attached
-	#joins(5)=((3,3),(2,0),(4,1),(6,0)) means crossing 5 is attached to crossings 3,2,4,5, at the strands specified
-	#basic non-binding consistency check implemented
-	def __init__(self,crossings,joins,name="nameless"):
-		self.crossings=tuple(crossings) #crossings: -1 (right strand up) or 1 (left strand up) (0 means a crossing has been deleted and can safely be ignored)
+	def __init__(self,crossings,joins):
+		self.crossings=tuple(crossings)
 		self.joins=joinstotuples(joins)
-		self.name=name
-		self.is_consistent()
 	def __key__(self):
 		return self.crossings,self.joins
 	def __hash__(self):
 		return hash(self.__key__())
 	def __eq__(self,other):
 		return self.__key__()==self.__key__()
-	def is_consistent(self):
-		if len(self.joins) != len(self.crossings):
-			print("Lengths of joins and crossings do not match in knot "+self.name)
-		for c,j in enumerate(self.joins):
-			if j!=0:
-				for i in range(4):
-					otherjoin=self.joins[j[i][0]]
-					if otherjoin==0:
-						print("Null reference at "+str((c,i))+" in knot "+self.name)
-						return False
-					if otherjoin[j[i][1]][0]!=c or otherjoin[j[i][1]][1]!=i:
-						print("Inconsistency at "+str((c,i))+" in knot "+self.name)
-						return False
-		return True
-	#perform a skein fork at crossing c and return two child knots
-	#The fork that should be multiplied by A in the Kauffman bracket calculation is returned first
-	def skein_fork(self,c,crosspos=None,canvlen=10,cnumber=False):
-		if sum(abs(i) for i in self.crossings)==1: return Link([],[],"unknot"),Link([],[],"unknot")
+	#Perform a skein fork at crossing c 
+	#and return two child knots.
+	#The fork that should be multiplied by A
+	#in the Kauffman bracket calculation is returned first
+	def skein_fork(self,c):
+		if sum(abs(i) for i in self.crossings)==1:
+			return Link([],[]),Link([],[])
 		
-		
-		#We generate two forks, order is determined by whether the crossing is over/under
+		#We generate two branches, order is 
+		#determined by whether the crossing is over/under
 		#F1: connects strands 1-3 and 0-2
 		#F2: connects strands 0-1 and 2-3
-		#sign 1: F1,F2 returned
-		#sign -1: F2,F1 returned
+		#overcrossing: F1,F2 returned
+		#undercrossing: F2,F1 returned
+		
 		j=self.joins[c]
-		if j==0:
-			print("You messed up, this should not happen!")
-			return self,self
-		cross0=j[0][0]
-		cross1=j[1][0]
-		cross2=j[2][0]
-		cross3=j[3][0]
-		othstr0=j[0][1]
-		othstr1=j[1][1]
-		othstr2=j[2][1]
-		othstr3=j[3][1]
 		f1crossings=list(self.crossings)
 		f2crossings=list(self.crossings)
 		f1joins=joinstolists(self.joins)
 		f2joins=joinstolists(self.joins)
 		f1crossings[c]=0
+		f2crossings[c]=0
 		if self.iseight(c):
 			f1joins[c]=0
-			return Link(f1crossings,f1joins,self.name+'1'),None
-		f2crossings[c]=0
-		
+			return Link(f1crossings,f1joins),None
 		loop=self.isloop(c)
-		
-			 
-		#connect 1 and 3
-		
-		f1joins[cross1][othstr1]=j[3]
-		f1joins[cross3][othstr3]=j[1]
-		#connect 0 and 2
-	
-		
-		f1joins[cross0][othstr0]=j[2]
-		f1joins[cross2][othstr2]=j[0]
-		
-		#connect 0 and 1
-		
-		f2joins[cross1][othstr1]=j[0]
-		f2joins[cross0][othstr0]=j[1]
-		#connect 2 and 3
-		f2joins[cross3][othstr3]=j[2]
-		f2joins[cross2][othstr2]=j[3]
 		if loop is not None:
-			
-			if loop==(0,2):
-				f2joins[cross1][othstr1]=j[3]
-				f2joins[cross3][othstr3]=j[1]
-			if loop==(1,3):
-				f2joins[cross0][othstr0]=j[2]
-				f2joins[cross2][othstr2]=j[0]
-			if loop==(2,3):
-				f1joins[cross0][othstr0]=j[1]
-				f1joins[cross1][othstr1]=j[0]
-			if loop==(0,1):
-				f1joins[cross2][othstr2]=j[3]
-				f1joins[cross3][othstr3]=j[2]
+			#Single-looped crossings leave only one option
+			#since we ignore unknot diagrams
+			toconnect=loopdic[loop]
+			connect(f1joins,j,toconnect[0],toconnect[1])
+			connect(f1joins,j,toconnect[1],toconnect[0])
+			f1joins[c]=0
+			res=Link(f1crossings,f1joins)
+			return res,None
+		#Dissolve the crossing and 
+		#reconnect broken strands for both branches
+		connect_all(f1joins,j,f1conn)
+		connect_all(f2joins,j,f2conn)
 		f1joins[c]=0
 		f2joins[c]=0
-		F1=Link(f1crossings,f1joins,self.name+'1')
-		F2=Link(f2crossings,f2joins,self.name+'2')
+		F1=Link(f1crossings,f1joins)
+		F2=Link(f2crossings,f2joins)
 		pfork,nfork=((F1,F2) if self.crossings[c]==1 else (F2,F1))
-		if crosspos is not None:
-			ltext=' loop untwisted' if loop is not None else ''
-			save_link(pfork,crosspos,canvlen,'A-branch'+ltext,cnumber)
-			save_link(nfork,crosspos,canvlen,'A^(-1)-branch'+ltext,cnumber)
 		return pfork,nfork
 	def isloop(self,c):
 		for i in range(len(self.joins[c])):
@@ -183,41 +152,47 @@ class Link(object):
 			if k[0]==c:
 				return (i,k[1]) if i<=k[1] else (k[1],i)
 		return None
+	#Determine whether crossing has two handles
 	def iseight(self,c):
 		j=self.joins[c]
-		return j==[(c,1),(c,0),(c,3),(c,2)] or j==[(c,2),(c,3),(c,0),(c,1)]
+		return j==[(c,1),(c,0),(c,3),(c,2)] or\
+				j==[(c,2),(c,3),(c,0),(c,1)]
 	def __str__(self):
 		return str(self.crossings)+'\n'+str(self.joins)
-	def kauffman(self,crosspos=None,canvlen=10,cnumber=False):
-		#use skein relations to compute the Kauffman bracket.
-		#check if we know this one already
+	def kauffman(self):
+		#Keep track of sub-polynomials we already computed.
 		res=seendic.get(self)
 		if res is not None:
 			return res
 		res=LaurentPolynomial([1])
-		nonzero=[i for i in range(len(self.crossings)) if self.crossings[i]!=0]
+		nonzero=[i for i in range(len(self.crossings))\
+				if self.crossings[i]!=0]
 		if len(nonzero)!=0:
 			c=nonzero[0]
 			if self.crossings[c]!=0:
-				#detect unknot on branch
 				loop=self.isloop(c)
 
-				pfork,nfork=self.skein_fork(c,crosspos,canvlen,cnumber)
-				pkauf=pfork.kauffman(crosspos,canvlen,cnumber)
-				nkauf=nfork.kauffman(crosspos,canvlen,cnumber)
+				pfork,nfork=self.skein_fork(c)
+				pkauf=pfork.kauffman()
 				if loop is not None:
+					#Account for R1-moves
 					#overtwist: -A**3
 					#undertwist: -A**(-3)
-					if (self.crossings[c]==-1 and (loop==(2,3) or loop==(0,1))) or (self.crossings[c]==1 and (loop==(0,2) or loop==(1,3))):
+					if  (self.crossings[c]==-1 and \
+							(loop==(2,3) or loop==(0,1))) \
+						or \
+						(self.crossings[c]==1 and \
+								(loop==(0,2) or loop==(1,3))):
 						res=-pkauf.amul(3)
 					else:
 						res=-pkauf.amul(-3)
 				else:
+					nkauf=nfork.kauffman()
 					res=(pkauf.amul(1)+nkauf.amul(-1))
 		seendic[self]=res
 		return res
 	def writhe(self):
-		#This routine doesn't work for links
+		#Assume we are operating on a knot
 		if sum(abs(j) for j in self.crossings)==0: return 0
 		
 		outputs=[[-1,-1] for i in range(len(self.crossings))]
@@ -227,8 +202,9 @@ class Link(object):
 		while curcrossing!=0 or instrand!=3:
 			
 			outstrand=inoutdic[instrand]
-			#print(curcrossing,instrand,outstrand)
-			outputs[curcrossing][0 if outputs[curcrossing][0]==-1 else 1]=outstrand
+			outputs[curcrossing][
+					0 if outputs[curcrossing][0]==-1 else 1
+					]=outstrand
 			instrand=self.joins[curcrossing][outstrand][1]
 			curcrossing=self.joins[curcrossing][outstrand][0]
 			
@@ -246,21 +222,3 @@ class Link(object):
 		correct_exp=-3*wr
 		res=kauff.amul(correct_exp)
 		return res if wr%2==0 else -res
-			
-def read_link(fname):
-	joins=[]
-	crossings=[]
-	with open(fname,'r') as infile:
-		for line in infile:
-			if ':' not in line or '[' not in line: continue
-			spl=line.split(':')
-			crossings.append(-1 if ('-' in spl[0]) else 1)
-			joins.append(literal_eval(spl[1].strip()))
-	return crossings,joins
-if __name__=='__main__':
-	righttrefoil=Link(*read_link('rtrefoil.txt'),name='rtrefoil')
-	print('right trefoil:',righttrefoil.xlpol())
-	kinotera=Link(*read_link('kinotera.txt'),name='Kinoshita-Terasaka')
-	print('Kinoshita-Terasaka: ',kinotera.xlpol())
-	conway=Link(*read_link('conway.txt'), name='Conway')
-	print('Conway: ',conway.xlpol())
